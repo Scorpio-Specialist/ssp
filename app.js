b9f893ab03fc8a1821fa6e226d978c5010a17ba7
waji <awaisdon1010101010@gmail.com>
2026-04-04 23:22:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors())
+ app.use(cors({
+  origin: "https://ceo-k7mv.vercel.app",
+  credentials: true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
